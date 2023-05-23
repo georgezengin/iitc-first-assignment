98cf6f51175c3619650b64183b6a64a41a6d9830
georgezengin <george.zengin@gmail.com>
2023-05-23 11:48:55 +0300

    התחקמות

--- third.py
+++ third.py
@@ -7,17 +7,24 @@
 # Please type a word: Learn
 # You entered the work Learn twice. Good bye…
 
-def wordcount(wrds, wd):
-    n = sum(1 for w in words if w.lower() == wd.lower())
-    return n
-
 words = []
 while True:
+    try:
+        maxtimes = int(input('Enter max number of times: '))
+        if maxtimes > 0:
+            break
+        if maxtimes < 0:
+            exit(0)
+    except ValueError:
+        print('*** Error *** Invalid number of times, try again ***')
+
+
+while True:
     w = input('Please type a word: ')
-    words.append(w)
-    n = wordcount(words,w)
-    if n>2:
-        print(f'"{w}" is repeated {n} time(s)')
+    words.append(w.lower())
+    n = words.count(w.lower())
+    if n >= maxtimes:
+        print(f'You entered the word "{w}" {n} time'+('s' if n > 1 else ''))
         break
 
 print('\nhasta la vista, baby!')
