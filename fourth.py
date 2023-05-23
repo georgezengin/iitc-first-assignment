62e256bf386d97dcae7e55af16aff4d49c34b54e
georgezengin <george.zengin@gmail.com>
2023-05-23 21:03:17 +0300

    extend fourth.py logic

--- fourth.py
+++ fourth.py
@@ -11,21 +11,23 @@
 # * אתגר : התוכל לפתור את התרגיל עבור רשימות באורך שונה )ספור רק את כמות האיברים
 # שמכילה הר שימה הקטנה: לדוגמא אם רשימה 1 מכילה 4 איברים ו רשימה 8 מכילה 8 איברים אז
 # השווה את 4 האיברים הראשונים בלבד( 
- 
-r1 = [ 3, 5, 30, 0]
-r2 = [1000, 5, 29, -5, 10, 20]
+
+r1 = [   3, 5, 30,  0 ]
+r2 = [1000, 5, 29, -5, 10, None, 20]
 
 n1 = 0
 n2 = 0
+
+# Note: I extended the logic to consider extra values in one of the arrays as greater than the other if value is not None, so
+#       code will iterate to max length of both arrays
 
 for i in range(max(len(r1), len(r2))):
     if i < len(r1) and i < len(r2): #while being in the range
         n1 += 1 if r1[i] > r2[i] else 0
         n2 += 1 if r2[i] > r1[i] else 0
-    elif i >= len(r1) and i < len(r2): #if size of r1 has ended
-        n2 += 1
-    elif i >= len(r2) and i < len(r1): #if size of r2 has ended
-        n1 += 1
+    else: #if size of r1 or r2 has ended
+        n1 += 1 if i >= len(r2) and r1[i] else 0 #consider any value in r1 to be greater than nothing
+        n2 += 1 if i >= len(r1) and r2[i] else 0 #consider any value in r2 to be greater than nothing
     
 print(f'List {r1} has {n1} numbers greater than list {r2}')
 print(f'List {r2} has {n2} numbers greater than list {r1}')
